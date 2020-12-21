fff766b44ff8508139ff6ce1d4df8067fea11281
Fortune Adekogbe <fortuneadekogbe@gmail.com>
2020-12-21 23:22:23 +0100

    Update bits_of_gray.py

--- day 4/bits_of_gray.py
+++ day 4/bits_of_gray.py
@@ -1,6 +1,14 @@
-def bits_of_gray(n):
+def bits_of_gray(n: int)->list:
+  """
+  This function returns the corresponding gray code for a specified number of bits.
+  """
+  try:
+    assert type(n)==int,"Invalid input"
+  except AssertionError as e:
+    return e
   if n==1:
     return ['0', '1']
   else:
     prev = bits_of_gray(n-1)
     return [f'0{i}' for i in prev] + [f'1{i}' for i in prev[::-1]]
+print(bits_of_gray(3))
